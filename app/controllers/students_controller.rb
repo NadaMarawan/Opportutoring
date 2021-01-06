262285ece03d1c43174fbac56d93095e2b721e11
Nada Marawan <nada.marwan@mail.mcgill.ca>
2021-01-06 14:01:34 -0500

    Add routes, controller and initial view for student sign up

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,41 @@
+class StudentsController < ApplicationController
+  
+  def show
+    @student = Student.find(params[:id])
+  end
+  
+  #This action will render app/views/students/new.html.erb
+  def new
+    #initialize but not save an empty student; so that the form we renders knows which fields to use and where to submit
+    @student= Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      # go to show page for @student
+      redirect_to action: "show", id: @student.id
+      # goes to new form
+      #redirect_to action: "new" 
+      #redirect_to students_path(@student)
+      #redirect_to @student
+      #redirect_to action: "show"
+    else
+      # can setup an error message here
+      render :new
+    end
+
+  end
+
+  # helper
+  private
+  # require a student object to be in "params" object (= object that contains all parameters being passed into the request)
+  # also requires (only) specified paramaters to exits in "params" object 
+  # permit some attributes to be used in the returned hash (whitelist)
+  # if checks are passed, returns a hash that is used here to create or update a student object
+    def student_params
+      params.require(:student).permit(:name,:password,:country, :level, :email)
+    end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html\
+  
+  # can add root route
+
+  # UI mockup page: signup-student
+  get '/students/new', to: 'students#new'
+  get "/students/:id", to: "students#show"
+  post '/students', to: 'students#create'
+  #resources :students
+   
 end
